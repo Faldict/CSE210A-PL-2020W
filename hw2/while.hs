7585c3070181f0713855e304fdd252c3e552f5f9
Faldict <legadoubledyan@gmail.com>
2020-01-18 18:03:11 -0800

    evaluate and show

--- hw2/while.hs
+++ hw2/while.hs
@@ -1,15 +1,19 @@
+import Data.Map
+import qualified Data.Map as Map
 import Control.Applicative((<*))
+import Control.Monad.State
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
 
--- cite code from Haskell wiki
+-- cite some parsec code from Haskell wiki
 type Name = String 
 data Expr = Var Name 
           | Con Bool
           | Nv Integer 
+          | Neg Expr
           | Not Expr 
           | Eq Expr Expr
           | Less Expr Expr
@@ -28,25 +32,61 @@ data Expr = Var Name
           | Seq [Expr]
     deriving Show
 
--- data Stmt = Skip | Name := Expr | If Expr Stmt Stmt | While Expr Stmt
---           | Seq [Stmt]
---     deriving Show
--- lexeme :: Parser a -> Parser a 
--- lexeme p = p <* whitespace
+type Store = Map Name Integer
+showStore :: Store -> String
+showStore e = case toList e of
+    [] -> "{}"
+    s -> "{" ++ (foldl1 showTwo (Prelude.map showKeyValue s)) ++ "}"
+              where showKeyValue (k, a) = k ++ " → " ++ (show a)
+                    showTwo a b = a ++ ", " ++ b
+
+evalB :: Store -> Expr -> Bool
+evalB e (Con b) = b
+evalB e (Not b) = not (evalB e b)
+evalB e (Eq a b) = evalI e a == evalI e b
+evalB e (Less a b) = evalI e a < evalI e b
+evalB e (Leq a b) = evalI e a <= evalI e b
+evalB e (Greater a b) = evalI e a > evalI e b
+evalB e (Geq a b) = evalI e a >= evalI e b
+evalB e (And a b) = evalB e a && evalB e b
+evalB e (Or a b) = evalB e a || evalB e b 
+
+evalI :: Store -> Expr -> Integer
+evalI e (Nv x) = x
+evalI e (Neg x) = negate $ evalI e x
+evalI e (Var x) = findWithDefault 0 x e
+evalI e (Mul p q) = evalI e p * evalI e q
+evalI e (Add p q) = evalI e p + evalI e q
+evalI e (Sub p q) = evalI e p - evalI e q
+
+eval :: Store -> Expr -> Store
+eval e (Skip) = e
+eval e (IfThenElse b p q) = case evalB e b of
+                        True -> eval e p
+                        False -> eval e q
+eval e (WhileDo b p) = case evalB e b of
+                   True -> eval e (Seq [p, WhileDo b p])
+                   False -> e
+eval e (Assign v a) = union e' e
+                where e' = insert v (evalI e a) Map.empty
+eval e (Seq []) = Map.empty
+eval e (Seq (x:xs)) = union (eval e' (Seq (xs))) e'
+                where e' = eval e x
 
 def = emptyDef{ commentStart = "{-"
               , commentEnd = "-}"
               , identStart = letter
               , identLetter = alphaNum
-              , opStart = oneOf "=:+-*<>¬∧"
-              , opLetter = oneOf "=:<>+-*¬∧"
-              , reservedOpNames = ["∧", "=", ":=", "+", "-", "*", "<", "<=", ">", ">=", "¬"]
+              , opStart = oneOf "=:+*<>¬∧"
+              , opLetter = oneOf "=:<>+-*¬∧∨"
+              , reservedOpNames = ["-", "∨", "∧", "=", ":=", "+", "*", "<", "<=", ">", ">=", "¬"]
               , reservedNames = ["true", "false", "skip",
                                  "if", "then", "else",
                                  "while", "do"]
               }
 
 TokenParser{ parens = m_parens
+           , braces = m_braces
            , identifier = m_identifier
            , reservedOp = m_reservedOp
            , reserved = m_reserved
@@ -57,6 +97,7 @@ exprparser :: Parser Expr
 exprparser = buildExpressionParser table term <?> "expression"
 
 table = [ [ Prefix (m_reservedOp "¬" >> return (Not))]
+        , [ Prefix (m_reservedOp "-" >> return (Neg))]
         , [ binaryL "*" (Mul) ]
         , [ binaryL "+" (Add)
           , binaryL "-" (Sub)]
@@ -76,12 +117,11 @@ table = [ [ Prefix (m_reservedOp "¬" >> return (Not))]
 
 term = m_parens exprparser
        <|> (fmap Var m_identifier)
-       <|> (m_reserved "true" >> return (Con True))
-       <|> (m_reserved "false" >> return (Con False))
-       <|> do { s <- string "-" <|> return []
-              ; cs <- many1 digit
+       <|> (m_reserved "true" >> spaces >> return (Con True))
+       <|> (m_reserved "false" >> spaces >> return (Con False))
+       <|> do { cs <- many1 digit
               ; spaces
-              ; return $ Nv $ read (s ++ cs)
+              ; return $ Nv $ read cs
               }
 
 whileparser :: Parser Expr
@@ -98,23 +138,34 @@ whileparser = m_whiteSpace >> stmtparser <* eof
               <|> do { m_reserved "if"
                      ; b <- exprparser
                      ; m_reserved "then"
-                     ; p <- stmtparser
+                     ; p <- stmtparser <|> m_braces stmtparser
                      ; m_reserved "else"
-                     ; q <- stmtparser
+                     ; q <- stmtparser <|> m_braces stmtparser
                      ; return (IfThenElse b p q)
                      }
               <|> do { m_reserved "while"
                      ; b <- exprparser
                      ; m_reserved "do"
-                     ; p <- stmtparser
+                     ; p <- stmtparser <|> m_braces stmtparser
                      ; return (WhileDo b p)
                      }
-              <|> try exprparser
+              <|> exprparser
+
+getLines :: IO [String]
+getLines = do
+  x <- getLine
+  if x == ""
+    then return []
+    else do
+      xs <- getLines
+      return (x:xs)
 
 main :: IO ()
 main = do
-    a <- getLine
-    case parse whileparser "" a of
+    -- codes <- getLines
+    -- let code = foldl1 (\a b -> a ++ b) codes in
+        code <- getLine
+        case parse whileparser "" code of
              { Left err -> print err
-             ; Right ans -> print ans
+             ; Right expr -> putStrLn $ showStore $ eval Map.empty expr
              }
